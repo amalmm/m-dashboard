47ffa405c89e0206fa1b4a3165fed3923097cc2f
amal <amalramachandran8@gmail.com>
2024-10-03 02:03:42 +0530

    update

--- resources/views/dashboard/layout/nav.blade.php
+++ resources/views/dashboard/layout/nav.blade.php
@@ -4,6 +4,22 @@
         <div class="text-xl text-green-500 p-2 font-semibold">{{ config('app.name', 'Laravel') }}</div>
 
 
+        {{-- profile --}}
+        <a href="{{ route('dashboard.home.index') }}" >
+        <div class="mt-3 flex gap-2 items-center hover:bg-gray-100 " >
+           <div>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="w-16">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+              </svg>
+           </div>
+
+           <div >
+               <div class="text-sm font-semibold"> {{ Auth::user()->name }}</div>
+             <div class="text-sm">{{ Auth::user()->email }}</div>
+           </div>
+        </div>
+        </a>
+
         {{-- Navigation Links --}}
         <div class="mt-3" id="navLinks">
             <ul class="group p-2 space-y-2">
@@ -63,8 +79,9 @@
                     text-gray-900 text-md hover:bg-gray-100 p-2 active:bg-gray-200   rounded-md flex gap-2">
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" class="w-5">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                           </svg>
+
 
                     </div>
                     profile
