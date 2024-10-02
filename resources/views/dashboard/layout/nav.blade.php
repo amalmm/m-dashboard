968282dffffb40182b57f46cfa90f16bf6b0f847
amal <amalramachandran8@gmail.com>
2024-10-02 12:42:04 +0530

    fixed nav bar

---
+++ resources/views/dashboard/layout/nav.blade.php
@@ -0,0 +1,25 @@
+<div class="w-[300px] fixed top-0 bottom-0 lg:left-0 p-2 bg-black overflow-y-auto text-left text-gray-500  hidden lg:block  ">
+    {{-- Logo --}}
+    <div class="text-xl text-white p-2">{{ config('app.name', 'Laravel') }}</div>
+
+
+    {{-- Navigation Links --}}
+    <div class="mt-3" id="navLinks">
+        <ul class="group p-2 space-y-2">
+            <li>
+                <a href="{{ route('dashboard.home.index') }}"
+                   class="{{ request()->routeIs('dashboard.home.index') ? 'bg-blue-900' : 'bg-transparent' }}
+                   text-gray-300 text-md hover:bg-blue-900 p-2 active:bg-gray-700 block rounded-md">
+                    Home
+                </a>
+            </li>
+            <li>
+                <a href=""
+                   class="text-gray-300 text-md hover:bg-blue-900 p-2 active:bg-gray-700 block rounded-md">
+                    Profile
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+
